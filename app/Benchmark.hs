b1b7cf761501a8db6e80c93000e47d2110f8ece6
Tom Ellis <tom-git@jaguarpaw.co.uk>
2021-09-26 13:02:28 +0100

    Benchmarking works

---
+++ app/Benchmark.hs
@@ -0,0 +1,336 @@
+-- Copyright (c) Microsoft Corporation.
+-- MIT License
+
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE DeriveTraversable #-}
+
+module Benchmark where
+
+import qualified Data.Foldable
+import Data.Function (fix)
+import Data.List (intercalate)
+--import qualified GHC.Stats
+import qualified System.Clock as Clock
+import Text.Read (readMaybe)
+import Text.Printf (printf)
+import System.IO.Temp (createTempDirectory, emptyTempFile)
+import qualified System.Mem
+
+import qualified Tree
+
+data BenchmarkParams = BenchmarkParams
+  { runsToMinimizeOver :: Int
+  , minimumMeasurableTime_secs :: Double
+  , maximumTime_micro :: Double
+  , sizeScale :: Double
+  }
+
+data Algorithms a = Algorithms
+  { aIO              :: a
+  , aList            :: a
+  , aStreaming       :: a
+  , aStreamingBetter :: a
+  }
+  deriving (Functor, Foldable, Traversable)
+
+algorithms_ :: Algorithms (String, Tree.Tree -> IO (), String)
+algorithms_ = Algorithms
+  { aIO              = ("IO", Tree.printTreeIO, baseline)
+  , aList            = ("List", Tree.printTreeList, good)
+  , aStreaming       = ("Streaming", Tree.printTreeStreaming, prettyBad)
+  , aStreamingBetter = ("Streaming better", Tree.printTreeBetterStreaming, veryBad)
+  }
+  where
+      veryBad   = "red"
+      prettyBad = "orange"
+      good      = "web-green"
+      baseline  = "web-blue"
+      --paper     = "purple"
+
+fast :: BenchmarkParams
+fast = BenchmarkParams
+  { runsToMinimizeOver = 3
+  , minimumMeasurableTime_secs = 0.01
+  , maximumTime_micro = 1000
+  , sizeScale = 2
+  }
+
+full :: BenchmarkParams
+full = BenchmarkParams
+  { runsToMinimizeOver = 10
+  , minimumMeasurableTime_secs = 0.1
+  , maximumTime_micro = 1000 * 1000
+  , sizeScale = 1.1
+  }
+
+-- | This is the entry point to the module.  When run it will
+-- benchmark the algorithms on a random set of expressions.  The data
+-- from the run will be written out to a directory whose name is
+-- displayed at the end of the run.
+benchmark :: BenchmarkParams -> IO ()
+benchmark bps = do
+  benchmarksDir <- createTempDirectory "." "benchmarks"
+  results_genNames <- benchmarkResults 120 bps
+  flip mapM_ results_genNames $ \results' -> do
+    datasets <- flip mapM results' $ \((algorithmName, _, algorithmColor), results) -> do
+        let textOutput = flip concatMap results $ \(size, time) ->
+              show size ++ " " ++  show time ++ "\n"
+
+        filename <- emptyTempFile benchmarksDir (algorithmName ++ ".dat")
+
+        writeFile filename textOutput
+
+        return PlotDataset
+          { pdFile  = filename
+          , pdTitle = algorithmName
+          , pdColor = algorithmColor
+          , pdStyle = "7"
+          , pdSize  = "0.25"
+          }
+
+    makeGnuplot benchmarksDir "No name" datasets
+
+benchmarkResults :: Int
+                 -> BenchmarkParams
+                 -> IO [[((String, Tree.Tree -> IO (), String),
+                          [(Int, Double)])]]
+benchmarkResults maxSize bps = do
+  let algorithms = Data.Foldable.toList algorithms_
+  results <- benchmarkNoPlot bps algorithms maxSize
+  pure [results]
+
+benchmarkNoPlot :: BenchmarkParams
+                -> [(String, Tree.Tree -> IO (), string)]
+                -> Int
+                -> IO [((String, Tree.Tree -> IO (), string), [(Int, Double)])]
+benchmarkNoPlot bps algorithms logMaxSize = do
+  let allParams = algorithms
+
+  flip mapM (enumFrom1 allParams) $ \(i, algorithm_) -> do
+    let (algorithmName, algorithm, _) = algorithm_
+    results <- loop (iteratorOfList [26..logMaxSize]) [] $ \logMaxSize rest -> do
+
+      let size = round (1.1 ** fromIntegral logMaxSize)
+
+      -- We force the expression after generating it.  The Expr type
+      -- is strict, that is forcing it forces everything it contains,
+      -- therefore no time is wasted forcing it in the hashing
+      -- algorithm itself.  On the other hand adding this bang pattern
+      -- made absolutely no difference to the benchmarks.  Presumably
+      -- the expression is generated already in forced state.  But
+      -- it's nice to keep this here for clarity.
+
+      putStrLn ("Parameter set "
+                 ++ show i ++ "/" ++ show (length allParams)
+                 ++ " (" ++ algorithmName ++ ")")
+      putStrLn ("Generating expression ...")
+      let !tree = Tree.leftSkewed size
+      let !exprSize' = size
+      putStrLn ("done.  Size was " ++ show exprSize')
+
+      let minimumMeasureableTime_micro = minimumMeasurableTime_secs bps * 1000 * 1000
+
+      (repeats, firstStats) <- benchmarkUntil minimumMeasureableTime_micro
+                                              1
+                                              algorithm
+                                              tree
+
+      r <- benchmarkMore firstStats
+                         (runsToMinimizeOver bps - 1)
+                         repeats
+                         algorithm
+                         tree
+
+      let (n, mean_micro, tmin_micro, _, stddev_micro) = stats r
+          showFloat = printf "%.0f" :: Double -> String
+
+      putStrLn ("Count: "    ++ show n)
+      putStrLn ("Mean: "     ++ showFloat mean_micro ++ "us")
+      putStrLn ("Min: "      ++ showFloat tmin_micro ++ "us")
+      putStrLn ("Std dev: "  ++ showFloat stddev_micro ++ "us")
+
+      let done = tmin_micro > maximumTime_micro bps
+          tmin_secs = tmin_micro / (1000 * 1000)
+          rest' = (exprSize', tmin_secs):rest
+
+      pure $ (if done then Right else Left) rest'
+
+    pure (algorithm_, results)
+
+makeGnuplot :: FilePath -> String -> [PlotDataset] -> IO ()
+makeGnuplot benchmarksDir xlabel results = do
+  gnuplotFilename <- emptyTempFile benchmarksDir "benchmarks.gnuplot"
+  gnuplotPdfFilename <- emptyTempFile benchmarksDir "benchmarks-pdf.gnuplot"
+
+  let gnuplotFileContent = gnuplotFile xlabel results
+      (outputPdf, gnuplotPdfFileContent) = gnuplotFilePdf benchmarksDir xlabel results
+
+  writeFile gnuplotFilename gnuplotFileContent
+  writeFile gnuplotPdfFilename gnuplotPdfFileContent
+
+  putStrLn ("I put stuff in " ++ benchmarksDir ++ ".")
+  putStrLn "If you have an X server and you want a live graph view run:"
+  putStrLn ("gnuplot --persist " ++ gnuplotFilename)
+  putStrLn "If you want to generate a PDF run:"
+  putStrLn ("gnuplot " ++ gnuplotPdfFilename)
+  putStrLn ("You will find the output PDF in " ++ outputPdf)
+
+type AggregateStatistics = (Int, Double, Double, Double)
+
+stats :: AggregateStatistics -> (Int, Double, Double, Double, Double)
+stats (n, tsum, tsquaredsum, tmin) = (n, mean, tmin, variance, stddev)
+  where n' = fromIntegral n
+        mean     = tsum / n'
+        variance = tsquaredsum / n' - mean * mean
+        stddev   = sqrt variance
+
+-- This is probably the entry point you want to use to benchmark an
+-- algorithm on a list of expressions each read from a FilePath.
+--
+-- Runs algorithm on expression and produces aggregate timing
+-- statistics.
+--
+-- benchmarkOne will seq the result of `algorithm expression`.  It is
+-- the caller's responsibility to ensure that this causes *all*
+-- desired work to be performed.  If you're not sure on this point
+-- please ask the author.
+benchmarkOne :: Int
+             -> Integer
+             -> (e -> r)
+             -> e
+             -> IO AggregateStatistics
+benchmarkOne = benchmarkMore (0, 0, 0, infinity)
+  where infinity = 1e60
+
+benchmarkMore :: AggregateStatistics
+              -> Int
+              -> Integer
+              -> (e -> r)
+              -> e
+              -> IO AggregateStatistics
+benchmarkMore already samplesPerExpression iterationsPerSample algorithm expression =
+  times samplesPerExpression already $ \(n, !t, !tsquared, !minSoFar) -> do
+        System.Mem.performMajorGC
+        start <- Clock.getTime Clock.Monotonic
+        times iterationsPerSample () $ \() ->
+          evaluate algorithm expression
+        stop <- Clock.getTime Clock.Monotonic
+
+        let elapsed_micro = iterationsElapsed_micro / fromIntegral iterationsPerSample
+              where iterationsElapsed = Clock.diffTimeSpec stop start
+                    iterationsElapsed_nano = Clock.toNanoSecs iterationsElapsed
+                    iterationsElapsed_micro = fromIntegral iterationsElapsed_nano / 1e3
+
+        return (n + 1,
+                t + elapsed_micro,
+                tsquared + elapsed_micro * elapsed_micro,
+                min minSoFar elapsed_micro)
+
+benchmarkUntil :: Double
+               -> Integer
+               -> (e -> IO r)
+               -> e
+               -> IO (Integer, AggregateStatistics)
+benchmarkUntil minimumMeasurableTime_micro repeats f x = do
+  System.Mem.performMajorGC
+  start <- Clock.getTime Clock.Monotonic
+  times repeats () $ \() -> f x >> pure ()
+  stop <- Clock.getTime Clock.Monotonic
+
+  let iterationsElapsed_micro = fromIntegral iterationsElapsed_nano / 1e3
+        where iterationsElapsed = Clock.diffTimeSpec stop start
+              iterationsElapsed_nano = Clock.toNanoSecs iterationsElapsed
+
+      elapsed_micro = iterationsElapsed_micro / fromIntegral repeats
+
+  if iterationsElapsed_micro < minimumMeasurableTime_micro
+  then benchmarkUntil minimumMeasurableTime_micro (2 * repeats) f x
+  else pure (repeats,
+             (1, elapsed_micro, elapsed_micro * elapsed_micro, elapsed_micro))
+
+gnuplotFilePdf :: String
+               -> String
+               -> [PlotDataset]
+               -> (String, String)
+gnuplotFilePdf benchmarksDir xlabel results = (outputPdf, unlines [
+    "set terminal pdf font \"Helvetica,13\""
+  , "set output \"" ++ outputPdf ++ "\""
+  , gnuplotFile xlabel results
+  ])
+  where outputPdf = benchmarksDir ++ "/benchmark.pdf"
+
+gnuplotFile :: String -> [PlotDataset] -> String
+gnuplotFile xlabel results =
+  unlines [ "set xlabel \"Number of nodes in expression (" ++ xlabel ++ ")\""
+          , "set ylabel \"Time taken to hash all subexpressions (s)"
+          , "set format y '%.0se%S'"
+          , "set format x '%.0se%S'"
+--          , "set size 1,1"
+          , "set logscale xy 10"
+          , "set key left top"
+          , "plot " ++ intercalate ", " (fmap plotDataset results)
+          ]
+
+data PlotDataset = PlotDataset
+  { pdFile  :: String
+  , pdTitle :: String
+  , pdColor :: String
+  , pdStyle :: String
+  , pdSize  :: String
+  }
+
+plotDataset :: PlotDataset -> String
+plotDataset pd = intercalate " " [ quote (pdFile pd)
+                                 , "title " ++ quote (pdTitle pd)
+                                 , "lt rgb " ++ quote (pdColor pd)
+                                 , "pt " ++ pdStyle pd
+                                 , "ps " ++ pdSize pd ]
+  where quote s = "\"" ++ s ++ "\""
+
+-- We apply the argument to the function here.  If we do it at the
+-- call site then GHC may float it outside of the timing loop!
+-- Therefore it's important that this function not be inlined.
+-- It seems it's also important for it to return IO so as not to be
+-- floated outside the timing loop.
+{-# NOINLINE evaluate #-}
+evaluate :: (e -> a) -> e -> IO ()
+evaluate a e = let !_ = a e
+                     in return ()
+
+times :: (Ord a, Num a, Monad m) => a -> s -> (s -> m s) -> m s
+times n s f = times_f 0 s
+  where times_f m s_ =
+          if m >= n
+          then return s_
+          else do
+            s' <- f s_
+            times_f (m + 1) s'
+
+enumFrom1 :: [a] -> [(Int, a)]
+enumFrom1 = zip [1..]
+
+loop :: Monad m => Iterator m b -> a -> (b -> a -> m (Either a a)) -> m a
+loop iterator a f = do
+  mNext <- stepIterator iterator
+  case mNext of
+    Nothing -> pure a
+    Just (b, iteratorNext) -> do
+      ea <- f b a
+      case ea of
+        Left aNext -> loop iteratorNext aNext f
+        Right aDone -> pure aDone
+
+newtype Iterator m a = Iterator (m (Maybe (a, Iterator m a)))
+  deriving Functor
+
+stepIterator :: Iterator m a -> m (Maybe (a, (Iterator m a)))
+stepIterator (Iterator a) = a
+
+iteratorOfList :: Applicative m => [a] -> Iterator m a
+iteratorOfList = \case
+  []   -> Iterator (pure Nothing)
+  x:xs -> Iterator (pure (Just (x, iteratorOfList xs)))
+
+iteratorPure :: Applicative m => a -> Iterator m a
+iteratorPure a = Iterator (pure (Just (a, Iterator (pure Nothing))))
---
+++ app/Tree.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Tree where
+
+import qualified System.IO as IO
+import qualified Streaming.Prelude
+import           Control.Monad ((>=>))
+
+data Tree = Branch Tree Tree | Leaf Int
+
+
+data Stream f m r = Step !(f (Stream f m r))
+                  | forall a. Bind (Stream f m a) (a -> Stream f m r)
+                  | Effect (m (Stream f m r))
+                  | Return r
+
+instance (Functor f, Monad m) => Functor (Stream f m) where
+  fmap f = loop where
+    loop stream = case stream of
+      Return r -> Return (f r)
+      Effect m -> Effect (do {stream' <- m; return (loop stream')})
+      Step   g -> Step (fmap loop g)
+      Bind s k -> Bind s (fmap loop k)
+  a <$ stream0 = loop stream0 where
+    loop stream = case stream of
+      Return _ -> Return a
+      Effect m -> Effect (do {stream' <- m; return (loop stream')})
+      Step   f -> Step (fmap loop f)
+      Bind s k -> Bind s (fmap loop k)
+
+instance (Functor f, Monad m) => Applicative (Stream f m) where
+  pure = Return
+  streamf <*> streamx = do {f <- streamf; x <- streamx; return (f x)}
+  stream1 *> stream2 = Bind stream1 (const stream2)
+
+instance (Functor f, Monad m) => Monad (Stream f m) where
+  return = pure
+  (>>) = (*>)
+
+  stream >>= f = Bind stream f
+{-
+    loop stream where
+    loop stream0 = case stream0 of
+      Step fstr -> Step (fmap loop fstr)
+      Effect m  -> Effect (fmap loop m)
+      Return r  -> f r
+-}
+
+data Of a b = !a :> b deriving Functor
+
+yield :: Monad m => a -> Stream (Of a) m ()
+yield a = Step (a :> Return ())
+
+mapM_ :: Monad m => (a -> m x) -> Stream (Of a) m r -> m r
+mapM_ f = loop where
+  loop str = case str of
+    Return r -> return r
+    Effect m -> m >>= loop
+    Step (a :> as) -> f a *> loop as
+    Bind (Return n) k -> loop (k n)
+    Bind (Effect m) k -> do
+      r <- m
+      loop (Bind r k)
+    Bind (Step (a :> as)) k -> f a *> loop (Bind as k)
+    Bind (Bind m k1) k2 -> loop (Bind m (k1 >=> k2))
+
+-- Fully evaluated left-skewed tree
+leftSkewed :: Int -> Tree
+leftSkewed 0 = Leaf 0
+leftSkewed n = (Branch $! leftSkewed (n - 1)) (Leaf n)
+
+printTreeIO :: Tree -> IO ()
+printTreeIO = \case
+  Leaf i -> IO.hPutStrLn IO.stderr (show i)
+  Branch t1 t2 -> do
+    printTreeIO t1
+    printTreeIO t2
+
+printTreeList :: Tree -> IO ()
+printTreeList = Prelude.mapM_ (IO.hPutStrLn IO.stderr . show) . toList
+  where toList = \case
+          Leaf i -> [i]
+          Branch t1 t2 -> toList t1 ++ toList t2
+
+printTreeStreaming :: Tree -> IO ()
+printTreeStreaming = Streaming.Prelude.mapM_ (IO.hPutStrLn IO.stderr . show) . toStream
+  where toStream = \case
+          Leaf i -> Streaming.Prelude.yield i
+          Branch t1 t2 -> do
+            toStream t1
+            toStream t2
+
+printTreeBetterStreaming :: Tree -> IO ()
+printTreeBetterStreaming = Tree.mapM_ (IO.hPutStrLn IO.stderr . show) . toStream
+  where toStream = \case
+          Leaf i -> Tree.yield i
+          Branch t1 t2 -> toStream t1 >> toStream t2
